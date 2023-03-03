cc9878ae7178d717eafa5b18a11e211152f5b335
Dexter Ty <dexter.gets@gmail.com>
2023-03-02 19:50:48 -0500

    second 2 commit

--- app/tours_DB/tests.py
+++ app/tours_DB/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 import pytest
-
+from tours_DB.models import Customers
 # Create your tests here.
 def test_homepage_access():
     url = reverse('home')
     assert url == "/"
+
+   
